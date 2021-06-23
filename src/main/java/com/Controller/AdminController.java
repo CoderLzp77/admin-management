aed716c0b60cd862fb96fa0a99f60f7aa8a0ee11
xzfine <1923263683@qq.com>
2021-06-23 18:30:58 +0800

    测试

--- src/main/java/com/Controller/AdminController.java
+++ src/main/java/com/Controller/AdminController.java
@@ -1,4 +1,20 @@
 package com.Controller;
 
+import com.Utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "Admin")
+@CrossOrigin
+@RestController
 public class AdminController {
+    @Autowired
+    private R r;
+    @GetMapping("/Test")
+    public R Test(){
+        return r.error();
+    }
 }
--- src/main/java/com/Utils/R.java
+++ src/main/java/com/Utils/R.java
@@ -1,4 +1,54 @@
 package com.Utils;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class R {
+    private int status;
+    private String message;
+    private Object data;
+
+    public R success(Object data){
+        this.setMessage("成功");
+        this.setStatus(200);
+        this.setData(data);
+        return this;
+    }
+    public R error(){
+        this.setStatus(404);
+        this.setMessage("失败");
+        return this;
+    }
+    public R message(String message){
+        this.setMessage(message);
+        return this;
+    }
+    public R status(int status) {
+        this.setStatus(status);
+        return this;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
